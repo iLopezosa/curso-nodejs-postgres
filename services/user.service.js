7ca76c74d78be2947dc91f760b14fb012c507deb
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-04-30 11:25:52 +0200

    all up to migrations

--- services/user.service.js
+++ services/user.service.js
@@ -1,32 +1,35 @@
 const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres')
-
+const { models } = require('../libs/sequelize');
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const client = await getConnection();
-    const res = await client.query('SELECT * FROM tasks');
-    return res.rows;
+    const res = await models.User.findAll();
+    return res;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user)
+      throw boom.notFound('User not found');
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const updatedUser = await user.update(changes);
+    return updatedUser;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
